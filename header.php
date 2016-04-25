ae758d3e4d4883a9701a83cc203b8fd45fb3009f
wuttinunt <wuttinunt.th@gmail.com>
2016-04-25 16:20:27 +0700

    add Show Top Rang player

--- header.php
+++ header.php
@@ -4,26 +4,38 @@
 	<title></title>
 </head>
 <body>
-<div class="wrapper">
-	<!-- header -->
-	<div class="header"> 	
-		<img src="images/cover/cover3.jpg" width="930px">
-	</div>
-	<!-- menu -->
-	<div class="header"> 	
-		<div id=main>
-			<ul id=nav>
-				<li class=home><a href="index.php">หน้าแรก HOME</a></li>
-				<li class=guides><a href="#">ไกด์ GUIDES</a></li>
-				<li class=heroes><a href="#">ฮีโร่ HEROES</a></li>
-				<li class=item><a href="#">ไอเท็ม ITEMS</a></li>
-				<li class=stats><a href="stats_player.php">สถิติ STATS</a></li>
-				<li class=logout><a href="logout.php">LOGOUT</a></li>
+	<?php
+	require_once("connect.php");
+	?>
 
-
-			</ul>
+	<div class="wrapper">
+		<!-- header -->
+		<div class="header"> 	
+			<img src="images/cover/cover3.jpg" width="930px">
 		</div>
+		<!-- menu -->
+		<div class="header"> 	
+			<div id=main>
+				<ul id=nav>
+					<li class=home><a href="index.php">หน้าแรก HOME</a></li>
+					<li class=guides><a href="#">ไกด์ GUIDES</a></li>
+					<li class=heroes><a href="#">ฮีโร่ HEROES</a></li>
+					<li class=item><a href="#">ไอเท็ม ITEMS</a></li>
+					<li class=stats><a href="stats_player.php">สถิติ STATS</a></li>
+					<?php 
+					if(!isset($_SESSION['UserID']))
+					{
+						echo '<li class=logout><a href="login.php">Login</a></li>';
 
-	</div>
-</body>
-</html>
+					}else
+					echo '<li class=logout><a href="logout.php">LOGOUT</a></li>';
+
+					?>
+
+
+				</ul>
+			</div>
+
+		</div>
+	</body>
+	</html>
